12b0e514f7c998737a5dfa95c53d7ce5bd787a3e
Amel Johny <ameljohny.mail@gmail.com>
2021-04-15 18:29:09 +1000

    changed home to functional component

--- frontend/src/Pages/home.jsx
+++ frontend/src/Pages/home.jsx
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Home extends Component {
-  render () {
-    return (
-      <div>
-        <p>Home</p>
-      </div>
-    )
-  }
+export default function home () {
+  return (
+    <div>
+
+    </div>
+  )
 }
